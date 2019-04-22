5bd7e94d7a26c231f6fdec6102fd6f7de7a6c345
Kerry0911 <908941959@qq.com>
2019-04-22 15:23:58 +0800

    bb

---
+++ src/main/java/com/itrip/controller/JumpController.java
@@ -0,0 +1,36 @@
+package com.itrip.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JumpController {
+
+    @RequestMapping("index")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/tavernindex")
+    public String taverindex(){
+        return "tavern/index";
+    }
+
+
+    @RequestMapping("/travelindex")
+    public String travelindex(){
+        return "travel/index";
+    }
+
+
+    @RequestMapping("/strategyindex")
+    public String strategyindex(){
+        return "strategy/index";
+    }
+
+    @RequestMapping("/flyticket")
+    public String flyticket(){
+        return "flyTicket";
+    }
+
+}
